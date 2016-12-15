259a5e6293ebb0db64eb0de3e55af562619c5d7f
Robert Chiodi <rmc298@cornell.edu>
2016-12-14 22:42:22 -0500

    Modifications that provide working code as long as stable time steps are kept in mind. Switched to Gauss-Seidel for faster solution to Poisson equation. Now will validate against known results

--- 01_lid_driven_cav/src/01_Fortran90/sim.f90
+++ 01_lid_driven_cav/src/01_Fortran90/sim.f90
@@ -14,51 +14,67 @@ end module precision
 
 module time_info
 	use precision
-	integer :: iter
+	integer :: titer
 	real(DP) :: dt, time, tfin, write_freq
 
-end module time
+end module time_info
 
-module parameter
+module parameters
 	use precision
-	real(DP) :: dw, dh, Ulid, rho, mu, init_dt
+	real(DP) :: dw, dh, Ulid, rho, mu, init_CFL
 	integer :: nx, ny ,ng
+	character(len=80) :: fstart
 
-end module parameter
+end module parameters
 
 module geometry
 	use precision
 	real(DP), dimension(:), allocatable :: x, y, xm, ym
-	real(DP) :: dx, dy, area, iarea
-	integer :: iming,imaxg,jming,jmaxg
-	integer :: imin, imax, jmin, jmax
+	real(DP) :: dx, dy, idx, idy, area, iarea
 
 end module geometry
 
+module indices
+
+	integer :: iming,imaxg,jming,jmaxg
+	integer :: imin, imax, jmin, jmax
+
+end module indices
+
 module operators
 	use precision
-	! Derivatives
+	! Derivatives for divergence of cell
   real(DP), dimension(:,:,:), allocatable :: cnt_divx, cnt_divy
-	integer :: fwd_m, fwd_p, bck_m, bck_p, div_m, div_p
+	integer :: div_m, div_p
+	
+	! Derivatives for gradient of U or V cell
+	real(DP), dimension(:,:,:), allocatable :: cnt_gradx, cnt_grady
+	integer :: grad_m, grad_p
+	
+	! Derivatives for gradient of pressure (Neumann BC on boundaries used)
+	real(DP), dimension(:,:,:), allocatable :: lapx, lapy
+	integer :: lap_m, lap_p
+	
 
 	! Interpolation
 	real(DP), dimension(:,:,:), allocatable :: int_x_c, int_y_c
 	real(DP), dimension(:,:,:), allocatable :: int_c_x, int_c_y
-	integer :: intxc_m, intxc_p, intyc_m, intyc_p, intcx_m, intcx_p &
-						 intcy_m, intcy_p
+	integer :: intxc_m, intxc_p, intyc_m, intyc_p, intcx_m, intcx_p
+	integer :: intcy_m, intcy_p
 
 end module operators
 
 module data
 	use precision
 	real(DP), dimension(:,:), allocatable :: U, V, P
-	real(DP), dimension(:,:), allocatable :: Uold, Vold, Pold
+	real(DP), dimension(:,:), allocatable :: Uold, Vold
 
 end module data
 
 module scratch
 	use precision
 	real(DP), dimension(:,:), allocatable :: Fx, Fy
+	real(DP), dimension(:,:), allocatable, target :: tmp1, tmp2
 
 end module scratch
 
@@ -66,7 +82,7 @@ end module scratch
 !! and then starts the flow solver
 program main
 
-	call parameter_set
+	call parameters_set
 	call grid_make
 	call operator_gen
 	call data_init
@@ -79,41 +95,47 @@ end program main
 !! Normally, this would be set through an input file or a GUI,
 !! but for simplicity to start, these will be hardcoded here in this
 !! function.
-subroutine parameter_set
-	
+subroutine parameters_set
+	use parameters
+	use geometry
+	use time_info
+	implicit none
 	! Domain width
 	dw = 1.0_DP
 	! Cells across the width
-	nx = 100
+	nx = 32
 	! Domain height
 	dh = 1.0_DP
 	! Cells across the height
-	ny = 100
+	ny = 32
 	! Lid velocity
 	Ulid = 1.0_DP
 	! Density of fluid
-	rho = 1.0_WP
+	rho = 2500.0_DP
 	! Dynamic viscosity of fluid
-	mu = 0.01_DP
+	mu = 1.0_DP
 	
 	! Number of ghost cells on each boundary
 	ng = 2
 	
 	! Initial time step size
-	init_dt = 0.85_DP*min(dx,dy)/Ulid !85% of initial CFL
+	init_CFL = 0.5_DP !85% initial CFL
 	! End time of simulation
-	tfin = 1.0_DP
+	tfin = 1000.0_DP
 	! Time in between data dumps
-	write_freq = 0.05_DP
+	write_freq = 10.0_DP
+	! File name used for data written to file
+	fstart = 'data'
 
 	return
-end subroutine parameter_set
+end subroutine parameters_set
 	
 !> Sets up the configuration of the grid (x,y locations).
 !! Rectangular grid is assumed, with a staggered cell arrangement
 subroutine grid_make
-	use parameter
+	use parameters
 	use geometry
+	use indices
 	implicit none
 	
 	integer :: i,j
@@ -144,12 +166,12 @@ subroutine grid_make
 	
 	! Left cell faces
 	do i = iming,imaxg
-		x(i) = real(i-1,DP)*dx-0.5_WP*dw
+		x(i) = real(i-1,DP)*dx-0.5_DP*dw
 	end do
 	
 	! Bottom cell faces
 	do j = jming,jmaxg
-		y(j) = real(j-1,DP)*dy-0.5_WP*dh
+		y(j) = real(j-1,DP)*dy-0.5_DP*dh
 	end do
 	
 	! Cell centers
@@ -170,29 +192,10 @@ end subroutine grid_make
 subroutine operator_gen
 	use geometry
 	use operators
+	use indices
 	implicit none
-		
-	! Second order forward derivative, assume uniform mesh
-	fwd_m = 0
-	fwd_p = 2
-	allocate(fwd_diffx(fwd_m:fwd_p,iming-fwd_m:imaxg-fwd_p,jming-fwd_m,jmaxg-fwd_p))
-	allocate(fwd_diffy(fwd_m:fwd_p,iming-fwd_m:imaxg-fwd_p,jming-fwd_m,jmaxg-fwd_p))
-	fwd_diffx(0,:,:) = -1.5_DP
-	fwd_diffx(1,:,:) =  2.0_DP
-	fwd_diffx(2,:,:) = -0.5_DP
-	fwd_diffy = fwd_diffx*idy
-	fwd_diffx = fwd_diffx*idx
 	
-	! Second order backward derivative, assume uniform mesh
-	bck_m = -2
-	bck_p =  0
-	allocate(bck_diffx(bck_m,bck_p,iming-bck_m:imaxg-bck_p,jming-bck_m:jmaxg-bck_p))
-	allocate(bck_diffy(bck_m:bck_p,iming-bck_m:imaxg-bck_p,jming-bck_m:jmaxg-bck_p))
-	bck_diffx(-2,:,:) =  0.5_DP
-	bck_diffx(-1,:,:) = -2.0_DP
-	bck_diffx( 0,:,:) =  1.5_DP
-	bck_diffy = bck_diffx*idy
-	bck_diffx = bck_diffx*idx
+	integer :: i,j
 	
 	! Second order centered derivative, assume uniform mesh
 	! Used to calculate divergence
@@ -200,42 +203,72 @@ subroutine operator_gen
 	div_p = 1
 	allocate(cnt_divx(div_m:div_p,iming-div_m:imaxg-div_p,jming-div_m:jmaxg-div_p))
 	allocate(cnt_divy(div_m:div_p,iming-div_m:imaxg-div_p,jming-div_m:jmaxg-div_p))
-	cnt_divx(0,:,:) = -0.5_DP
-	cnt_divx(1,:,:) =  0.5_DP
-	cnt_divy = cnt_diffx*idy
-	cnt_divx = cnt_diffx*idx
+	cnt_divx(0,:,:) = -1.0_DP
+	cnt_divx(1,:,:) =  1.0_DP
+	cnt_divy = cnt_divx*idy
+	cnt_divx = cnt_divx*idx
+	
+	! Second order centered derivative, assume uniform mesh
+	! Used to calculate gradient
+	grad_m = 0
+	grad_p = 1
+	allocate(cnt_gradx(grad_m:grad_p,iming-grad_m:imaxg-grad_p,jming-grad_m:jmaxg-grad_p))
+	allocate(cnt_grady(grad_m:grad_p,iming-grad_m:imaxg-grad_p,jming-grad_m:jmaxg-grad_p))
+	cnt_gradx(0,:,:) = -1.0_DP
+	cnt_gradx(1,:,:) =  1.0_DP
+	cnt_grady = cnt_gradx*idy
+	cnt_gradx = cnt_gradx*idx
+	
+	! Laplacian operator, assume uniform mesh
+	! Used for evaluation of lap(P), assumes Neumann BC on domain boundary
+	lap_m = -1
+	lap_p =  1
+	allocate(lapx(lap_m:lap_p,iming-lap_m:imaxg-lap_p,jming-lap_m:jmaxg-lap_p))
+	allocate(lapy(lap_m:lap_p,iming-lap_m:imaxg-lap_p,jming-lap_m:jmaxg-lap_p))
+	lapx(-1,:,:) =  1.0_DP
+	lapx( 0,:,:) =  -2.0_DP
+	lapx( 1,:,:) =  1.0_DP
+	lapy = lapx
+	! Add the Neumann BC on Boundaries
+	lapx(-1,imin,:) = 0.0_DP; lapx(0,imin,:) = -1.0_DP ! Left wall
+	lapx( 1,imax,:) = 0.0_DP; lapx(0,imax,:) = -1.0_DP ! Right wall
+	lapy(-1,:,jmin) = 0.0_DP; lapy(0,:,jmin) = -1.0_DP ! Bottom wall
+	lapy( 1,:,jmax) = 0.0_DP; lapy(0,:,jmax) = -1.0_DP ! Top wall
+	! Divide by dx2
+	lapy = lapy*idy*idy
+	lapx = lapx*idx*idx
 	
 	! Interpolation from X-face to cell center (First order)
 	intxc_m= 0
 	intxc_p = 1
 	allocate(int_x_c(intxc_m:intxc_p,iming-intxc_m:imaxg-intxc_p, &
 																	 jming-intxc_m:jmaxg-intxc_p))
-	int_x_c(0) = 0.5_DP
-	int_x_c(1) = 0.5_DP
+	int_x_c(0,:,:) = 0.5_DP
+	int_x_c(1,:,:) = 0.5_DP
 
 	! Interpolation from Y-face to cell center (First order)
 	intyc_m = 0
 	intyc_p = 1
 	allocate(int_y_c(intyc_m:intyc_p,iming-intyc_m:imaxg-intyc_p, &
 																	 jming-intyc_m:jmaxg-intyc_p))
-	int_y_c(0) = 0.5_DP
-	int_y_c(1) = 0.5_DP
+	int_y_c(0,:,:) = 0.5_DP
+	int_y_c(1,:,:) = 0.5_DP
 		
 	! Interpolation from cell center to X-face (First order)
 	intcx_m = -1
 	intcx_p =  0
-	allocate(int_c_x(intcx_m:intcx_p,iming-intcx_m,imaxg-intcx_p, &
-																	 jming-intcx_m,jmaxg-intcx_p))
-	int_c_x(-1) = 0.5_DP
-	int_c_x( 0) = 0.5_DP
+	allocate(int_c_x(intcx_m:intcx_p,iming-intcx_m:imaxg-intcx_p, &
+																	 jming-intcx_m:jmaxg-intcx_p))
+	int_c_x(-1,:,:) = 0.5_DP
+	int_c_x( 0,:,:) = 0.5_DP
 	
 	! Interpolation from cell center to Y-face (First order)
 	intcy_m = -1
 	intcy_p =  0
 	allocate(int_c_y(intcy_m:intcy_p,imaxg-intcy_m:imaxg-intcy_p, &
 																	 jming-intcy_m:jmaxg-intcy_p))
-	int_c_y(-1) = 0.5_DP
-	int_c_y( 0) = 0.5_DP
+	int_c_y(-1,:,:) = 0.5_DP
+	int_c_y( 0,:,:) = 0.5_DP
 	
 	return
 end subroutine operator_gen
@@ -243,7 +276,10 @@ end subroutine operator_gen
 !> Initialize the data that will be used in the simulation
 subroutine data_init
 	use data
+	use parameters
 	use geometry
+	use scratch
+	use indices
 	implicit none
 	
 	! Allocate the velocity in the x-direction, stored on X-faces
@@ -254,19 +290,22 @@ subroutine data_init
 	allocate(Vold(iming:imaxg,jming:jmaxg))
 	! Allocate the pressure, stored in the cell center
 	allocate(P(iming:imaxg,jming:jmaxg))
-	allocate(Pold(iming:imaxg,jming:jmaxg))
 	
 	! Scratch memory for fluxes
-	allocate(Fx(iming:imaxg,jming:jmaxg)
-	allocate(Fy(iming:imaxg,jming:jmaxg)
-	
+	allocate(Fx(iming:imaxg,jming:jmaxg))
+	allocate(Fy(iming:imaxg,jming:jmaxg))
+	! Other scratch memory arrays
+	allocate(tmp1(iming:imaxg,jming:jmaxg))
+	allocate(tmp2(iming:imaxg,jming:jmaxg))
 	
 	! Since this is lid driven cavity, initialize all to zero
 	! except for the lid velocity.
 	! Will use convention that cell (1,1) is in the bottom left.
 	U = 0.0_DP
 	V = 0.0_DP
-	P = 0.0_DP
+	Uold = 0.0_DP
+	Vold = 0.0_DP
+	P = 1.0_DP
 	
 	U(:,jmax+1:jmaxg) = Ulid
 
@@ -275,37 +314,50 @@ end subroutine data_init
 
 !> Advance the simulation through time
 subroutine flow_solve
-	use parameter
+	use parameters
 	use time_info
+	use geometry
 	implicit none
 	integer :: write_int
 
 	! Initialize the different simulation models
 	call velocity_init
 	call pressure_init
+	call monitor_init
+	call dump_init
 	
 	! Start simulation at T = 0.0, with dt = init_dt
 	time = 0.0_DP
-	write_int = 0
-	dt = init_dt
+	write_int = 1
+	dt = init_CFL*min(dx,dy)/Ulid
+	dt = min(dt,init_CFL*dx*dx*rho/(2.0_DP*mu))
+	
+	! Print initial monitor and write initial data to file
+	call monitor_write
+	call dump_data
 	
 	! Advance the simulation through time
+	titer = 0
 	do while(time.lt.tfin)
+		! Update iteration counter
+		titer = titer + 1
 	  ! Advance U from n to n+1/2
 		call velocity_solve
 		! Solve for pressure to cause solenoidal velocity field
 		call pressure_solve
 		! Correct velocity to the solenoidal field
 		call velocity_correct
+		! Advance the simulation time
+		time = time+dt
+		
 		! Monitor the simulation
-		call sim_monitor
+		call monitor_write
 		! Write out files for visualizing the simulation
 		if(floor(time/write_freq) .ge. write_int) then
 			write_int = write_int+1
-			call data_dump
+			call dump_data
 		end if
-		! Advance the simulation time
-		time = time+dt
+		
 	end do
 
 	return
--- 01_lid_driven_cav/src/01_Fortran90/velocity.f90
+++ 01_lid_driven_cav/src/01_Fortran90/velocity.f90
@@ -9,7 +9,7 @@ subroutine velocity_init
 	return
 end subroutine velocity_init
 
-subroutine velocity_solver
+subroutine velocity_solve
 	use data
 	implicit none
 	
@@ -24,12 +24,14 @@ subroutine velocity_solver
 	call velocity_solver_visc_v
 	
 	return
-end subroutine velocity_solver
+end subroutine velocity_solve
 
 subroutine velocity_solver_conv_u
 	use data
 	use operators
 	use scratch
+	use time_info
+	use indices
 	implicit none
 	
 	integer :: i,j
@@ -38,50 +40,195 @@ subroutine velocity_solver_conv_u
 	
 	! Loop and compute convective fluxes at left and bottom of U-cell
 	! Flux is U \vec{U}
+	Fx = 0.0_DP
+	Fy = 0.0_DP
 	do j = jmin,jmax+1
 		do i = imin, imax+1
-			Fx(i,j) = sum(int_x_c(:,i,j)*Uold(i-1+intxc_m:i-1+intxc_p,j))*&
-								sum(int_x_c(:,i,j)*Uold(i-1+intxc_m:i-1+intxc_p,j))
+			Fx(i,j) = -sum(int_x_c(:,i,j)*Uold(i-1+intxc_m:i-1+intxc_p,j))*&
+								 sum(int_x_c(:,i,j)*Uold(i-1+intxc_m:i-1+intxc_p,j))
 								
-			Fy(i,j) = sum(int_c_x(:,i,j)*Vold(i+intcx_m:i+intcx_p,j))*&
-								sum(int_c_y(:,i,j)*Uold(i,j+intcy_m:j+intcy_p))
+			Fy(i,j) = -sum(int_c_x(:,i,j)*Vold(i+intcx_m:i+intcx_p,j))*&
+								 sum(int_c_y(:,i,j)*Uold(i,j+intcy_m:j+intcy_p))
 		end do
 	end do
 	
 	! Take divergence with Fx and Fy for each cell, update U from n&
-	! to n+1/2. U(imin,:) is on the boundary
+	! to n+1/2. U(imin,:) is on the boundary and will be set with a BC
 	do j = jmin,jmax
 		do i = imin+1,imax
 			U(i,j) = Uold(i,j)+dt*(&
-												 sum(cnt_divx(:,i,j)*Fx(i+div_m:i+div_p,j))&
-												 sum(cnt_divy(:,i,j)*Fy(i,j+div_m:j+div_p))&
+												 sum(cnt_divx(:,i,j)*Fx(i+div_m:i+div_p,j))+&
+												 sum(cnt_divy(:,i,j)*Fy(i,j+div_m:j+div_p)) &
 																																	 )
 		end do
 	end do
 		
 	return
-end velocity_solver_conv_u
+end subroutine velocity_solver_conv_u
 
 subroutine velocity_solver_visc_u
 	use data
+	use parameters
 	use operators
 	use scratch
+	use time_info
+	use indices
 	implicit none
 	
 	integer :: i,j
 
 	! Calculating mu/rho * lap(U)
-	
+			
 	! Loop and compute viscous fluxes at left and bottom of U-cell
-	! Flux is mu*grad(U)
+	! Flux is mu/rho*grad(U)
+	Fx = 0.0_DP
+	Fy = 0.0_DP
 	do j = jmin, jmax+1
 		do i = imin, imax+1
-			Fx(i,j) = sum(int_x_c(:,i,j)*Uold(i+intxc_m:i+intxc_p,j))
+			Fx(i,j) = sum(cnt_gradx(:,i-1,j)*Uold(i-1+grad_m:i-1+grad_p,j))
+			Fy(i,j) = sum(cnt_grady(:,i,j-1)*Uold(i,j-1+grad_m:j-1+grad_p))
+		end do
+	end do
+	! Multiply fluxes by mu/rho
+	Fx = mu/rho*Fx
+	Fy = mu/rho*Fy
+		
+	! Take divergence with Fx and Fy for each cell, update U to include
+	! the viscous part. U(imin,:) is on the boundary, and will be set 
+	! with a BC
+	do j = jmin,jmax
+		do i = imin+1,imax
+			U(i,j) = U(i,j)+dt*(&
+							  	sum(cnt_divx(:,i,j)*Fx(i+div_m:i+div_p,j))+&
+					 			  sum(cnt_divy(:,i,j)*Fy(i,j+div_m:j+div_p)) &
+																														)
+		end do
+	end do
+	
+	return
+end subroutine velocity_solver_visc_u
+
+subroutine velocity_solver_conv_v
+	use data
+	use operators
+	use scratch
+	use time_info
+	use indices
+	implicit none
+	
+	integer :: i,j
+	
+	! Calculating div(V \vec{U})
+	
+	! Loop and compute convective fluxes at left and bottom of V-cell
+	! Flux is V \vec{U}
+	Fx = 0.0_DP
+	Fy = 0.0_DP
+	do j = jmin,jmax+1
+		do i = imin, imax+1
+			Fx(i,j) = -sum(int_c_y(:,i,j)*Uold(i,j+intcy_m:j+intcy_p))*&
+								 sum(int_c_x(:,i,j)*Vold(i+intcx_m:i+intcx_p,j))
+								
+			Fy(i,j) = -sum(int_y_c(:,i,j-1)*Vold(i,j-1+intyc_m:j-1+intyc_p))*&
+								 sum(int_y_c(:,i,j-1)*Vold(i,j-1+intyc_m:j-1+intyc_p))
+		end do
+	end do
+	
+	! Take divergence with Fx and Fy for each cell, update U from n&
+	! to n+1/2. U(imin,:) is on the boundary and will be set with a BC
+	do j = jmin+1,jmax
+		do i = imin,imax
+			V(i,j) = Vold(i,j)+dt*(&
+												 sum(cnt_divx(:,i,j)*Fx(i+div_m:i+div_p,j))+&
+												 sum(cnt_divy(:,i,j)*Fy(i,j+div_m:j+div_p)) &
+																																	 )
+		end do
+	end do
+		
+	return
+end subroutine velocity_solver_conv_v
+
+subroutine velocity_solver_visc_v
+	use data
+	use parameters
+	use operators
+	use scratch
+	use time_info
+	use indices
+	implicit none
+	
+	integer :: i,j
+
+	! Calculating mu/rho * lap(V)
+			
+	! Loop and compute viscous fluxes at left and bottom of U-cell
+	! Flux is mu*grad(V)
+	Fx = 0.0_DP
+	Fy = 0.0_DP
+	do j = jmin, jmax+1
+		do i = imin, imax+1
+			Fx(i,j) = sum(cnt_gradx(:,i-1,j)*Vold(i-1+grad_m:i-1+grad_p,j))
+			Fy(i,j) = sum(cnt_grady(:,i,j-1)*Vold(i,j-1+grad_m:j-1+grad_p))
+		end do
+	end do
+	! Multiply fluxes by mu
+	Fx = mu/rho*Fx
+	Fy = mu/rho*Fy
+	
+	! Take divergence with Fx and Fy for each cell, update U to include
+	! the viscous part. U(imin,:) is on the boundary, and will be set 
+	! with a BC
+	do j = jmin+1,jmax
+		do i = imin,imax
+			V(i,j) = V(i,j)+dt*(&
+							  	sum(cnt_divx(:,i,j)*Fx(i+div_m:i+div_p,j))+&
+					 			  sum(cnt_divy(:,i,j)*Fy(i,j+div_m:j+div_p)) &
+																														)
+		end do
+	end do
+	
+	return
+end subroutine velocity_solver_visc_v
+
+! Update velocity to solenoidal velocity field,
+! from time n^* to n+1
+subroutine velocity_correct
+	use data
+	use operators
+	use indices
+	use time_info
+	use pressure
+	implicit none
+	
+	integer :: i,j
+	
+
+	! Update U velocity
+	do j = jmin, jmax
+		do i = imin+1, imax
+			U(i,j) = U(i,j) - dt*sum(cnt_gradx(:,i-1,j)*&
+															 P(i-1+grad_m:i-1+grad_p,j))
+		end do
+	end do
+
+	! Update V velocity
+	do j = jmin+1, jmax
+		do i = imin, imax
+			V(i,j) = V(i,j) - dt*sum(cnt_grady(:,i,j-1)*&
+															 P(i,j-1+grad_m:j-1+grad_p))
 		end do
 	end do
 	
 	
+	!call pressure_RHS
+	!	do j = jmin, jmax
+	!	do i = imin, imax
+	!	print*,'RHS', i,j,RHS(i,j)
+	!	end do
+	!	end do
+	
 	
 	return
-end subroutine velocity_solver_visc_u
+end subroutine velocity_correct
+
 
